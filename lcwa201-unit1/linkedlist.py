94022692bd11e0f5d7d9afce0a0c8c1125cf43df
therogue <alex@roguemark.com>
2018-06-06 20:35:19 -0400

    Assignment 10

--- lcwa201-unit1/linkedlist.py
+++ lcwa201-unit1/linkedlist.py
@@ -5,8 +5,29 @@ class LinkedList:
         self.next = None
     
     def __str__(self):
-        return str(self.data)
+        if self.next:
+            return '[' + str(self.data) + ' HAS A NEXT] and ' + str(self.next)
+        return str(self.data) + '[DOES NOT].'
+    
+    def last(self):
+        return None     # Change this to return the last element of the list
 
-node = LinkedList(1)
 
+# main starts here -----------
+
+node = LinkedList('apples')
+
+print(node)
+
+nextnode = LinkedList('bananas')
+
+print(nextnode)
+
+node.next = nextnode
+
+print(node)
+
+nextnode.next = LinkedList('kiwis')
+
+print(nextnode)
 print(node)
